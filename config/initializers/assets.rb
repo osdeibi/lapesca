2d371c1904a6134467db60a8dd3e875c617e7d4d
Alan Salinas <alan.sagz@gmail.com>
2016-07-01 12:57:50 -0500

    Fixes asset pipeline using home.js and home.scss manifests including Lets Travel template assets for landing page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( *.css )
-Rails.application.config.assets.precompile += %w( *.js )
+Rails.application.config.assets.precompile += %w( home.scss )
+Rails.application.config.assets.precompile += %w( home.js )
 Rails.application.config.assets.precompile += %w( *.jpg )
 Rails.application.config.assets.precompile += %w( *.png )
