e15655ccd00e641a1715d888719211f144a7aeae
Alan Salinas <alan.sagz@gmail.com>
2016-07-10 17:02:42 -0500

    Fixes bug

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -8,61 +8,61 @@ class HotelsController < ApplicationController
   def la_pescalinda
     @hotel = Hotel.find(1) # Id 1: La Pescalinda
     get_recommended_similar
-    render 'hotel_detail.html.erb'
+    render 'show.html.erb'
   end
 
   def tropicana
     @hotel = Hotel.find(2)
     get_recommended_similar
-    render 'hotel_detail.html.erb'
+    render 'show.html.erb'
   end
 
   def palma_real
     @hotel = Hotel.find(3)
     get_recommended_similar
-    render 'hotel_detail.html.erb'
+    render 'show.html.erb'
   end
 
   def riviera_del_rio
     @hotel = Hotel.find(4)
     get_recommended_similar
-    render 'hotel_detail.html.erb'
+    render 'show.html.erb'
   end
 
   def nuevo_amanecer
     @hotel = Hotel.find(5)
     get_recommended_similar
-    render 'hotel_detail.html.erb'
+    render 'show.html.erb'
   end
 
   def marina_del_rio
     @hotel = Hotel.find(6)
     get_recommended_similar
-    render 'hotel_detail.html.erb'
+    render 'show.html.erb'
   end
 
   def villa_del_mar
     @hotel = Hotel.find(9)
     get_recommended_similar
-    render 'hotel_detail.html.erb'
+    render 'show.html.erb'
   end
 
   def la_escollera
     @hotel = Hotel.find(8)
     get_recommended_similar
-    render 'hotel_detail.html.erb'
+    render 'show.html.erb'
   end
 
   def punta_piedra
     @hotel = Hotel.find(10)
     get_recommended_similar
-    render 'hotel_detail.html.erb'
+    render 'show.html.erb'
   end
 
   def maria_esperanza
     @hotel = Hotel.find(11)
     get_recommended_similar
-    render 'hotel_detail.html.erb'
+    render 'show.html.erb'
   end
 
   def show
