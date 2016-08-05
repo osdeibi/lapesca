5f64ab14e1af5be8f94f4252a232eb850c2368da
Alan Salinas <alan.sagz@gmail.com>
2016-08-05 11:52:43 -0500

    update

--- config/initializers/email_setup.rb
+++ config/initializers/email_setup.rb
@@ -1,11 +1,11 @@
-if Rails.env.staging? || Rails.env.production?
+if true # Rails.env.staging? || Rails.env.production?
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['GMAIL_USERNAME'],
     :password => ENV['GMAIL_PASSWORD'],
     :domain => 'gmail.com',
     :address => "smtp.gmail.com",
     :port => 587,
-    :authentication => :login, # plain?
+    :authentication => :plain, # plain?login
     :enable_starttls_auto => true
   }
 end
