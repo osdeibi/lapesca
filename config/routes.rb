f59cb133a11b7f6e346220940206eefa5316da77
Alan Salinas <alan.sagz@gmail.com>
2016-08-23 19:52:56 -0500

    Adds weather

--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,7 @@ Rails.application.routes.draw do
   get '/tortuga_lora-lapescatamaulipas', to: 'pages#tortuga_lora', as: 'tortuga_lora'
   get '/tu_reservacion', to: 'pages#thank_you', as: 'thank_you'
   get '/el_sabinito', to: 'pages#el_sabinito', as: 'el_sabinito'
+  get '/clima', to: 'pages#weather', as: 'weather'
 
   get '/dashboard', to: 'dashboard#reservations', as: 'dashboard_index'
   get '/dashboard/calendar/:id', to: 'dashboard#calendar', as: 'calendar'
