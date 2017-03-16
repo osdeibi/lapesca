3d5a92e93bf0d2685f9d7d5f23e8d97a79596221
Ezequiel <ezedepetris@gmail.com>
2017-03-15 23:24:41 -0300

    remove table user

---
+++ db/migrate/20170316022306_remove_table_user.rb
@@ -0,0 +1,5 @@
+class RemoveTableUser < ActiveRecord::Migration
+  def change
+    drop_table :users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316004528) do
+ActiveRecord::Schema.define(version: 20170316022306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,11 +102,6 @@ ActiveRecord::Schema.define(version: 20170316004528) do
   end
 
   add_index "rooms", ["hotel_id"], name: "index_rooms_on_hotel_id", using: :btree
-
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   add_foreign_key "reservations", "hotels"
   add_foreign_key "reservations", "rooms"
