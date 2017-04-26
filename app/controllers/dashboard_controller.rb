36076169e4e4ead74cb681484f2e7a0a6ab3aaf7
Ezequiel <ezedepetris@gmail.com>
2017-04-26 17:38:37 -0300

    refactor controller dashboard calendar and css calendar

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   before_action :set_hotels
-  http_basic_authenticate_with name: "hdlgarza", password: ENV['GMAIL_PASSWORD']
+  http_basic_authenticate_with name: "hdlgarza", password: "hdlgarza"
 
   def index
   end
@@ -18,8 +18,8 @@ class DashboardController < ApplicationController
 
   def calendar
     @hotel = Hotel.find(params[:id])
-    @calendar_dates = CalendarDate.where(@hotel.code => true) # true marks full capacity
-    @dates = Array.new
+    @calendar_dates = @hotel.calendar_dates
+    @dates = []
     @calendar_dates.each do |cd|
       @dates.push({:title => 'Lleno [x]', :start => cd.actual_date, :url => clear_calendar_date_path(@hotel, cd)})
     end
@@ -27,21 +27,22 @@ class DashboardController < ApplicationController
 
   def clear_calendar_date
     @hotel = Hotel.find(params[:id])
-    @cd = CalendarDate.find(params[:calendar_date_id])
-    @cd.update(@hotel.code => false) # full capacity: false
+    @hotel.calendar_dates.find(params[:calendar_date_id]).destroy
+    @dates = []
+    @hotel.calendar_dates.each do |cd|
+      @dates.push({:title => 'Lleno [x]', :start => cd.actual_date, :url => clear_calendar_date_path(@hotel, cd)})
+    end
     redirect_to :calendar
   end
 
   def fill_calendar_date
     @hotel = Hotel.find(params[:id])
-    cd = Date.parse(params[:started])
-    @cd = CalendarDate.find_by(actual_date: cd)
+    date = Date.parse(params[:started])
 
-    if @cd
-      @cd.update(@hotel.code => true) # full capacity: true
-    else
-      @cd = CalendarDate.create(@hotel.code => true, :actual_date => cd)
+    if @hotel.calendar_dates.where(actual_date: date).blank?
+      @hotel.calendar_dates.create(actual_date: date)
     end
+
     redirect_to :calendar
   end
 
